d2dddf2883176a08a6a597cfb7d423d3041d7921
jung su young <j9972@naver.com>
2022-01-14 16:37:18 +0900

    home: change class component to funtion component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Movie from "../components/Movie";
 import "./Home.css";
 
-class Home extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    //movies.data.data.movies == {data: {data: { movies } } }
+function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
     const {
       data: {
         data: { movies },
@@ -18,40 +16,41 @@ class Home extends React.Component {
       "https://yts-proxy.now.sh/list_movies.json?sort_by=like_count"
     );
     //this.setState({movies:movies, isLoading: false})
-    this.setState({ movies, isLoading: false });
+    // this.setState({ movies, isLoading: false });
+    setMovies(movies);
+    setIsLoading(false);
   };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading..</span>
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  // const { isLoading, movies } = this.state;
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading..</span>
+        </div>
+      ) : (
+        <div>
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+                rating={movie.rating}
+              />
+            ))}
           </div>
-        ) : (
-          <div>
-            <div className="movies">
-              {movies.map((movie) => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                  rating={movie.rating}
-                />
-              ))}
-            </div>
-          </div>
-        )}
-      </section>
-    );
-  }
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Home;
